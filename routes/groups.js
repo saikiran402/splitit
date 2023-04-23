c5358b6d269e81e1405328f19212624935267a74
Saikiran <sai.kiran3500@gmail.com>
2023-04-23 02:15:11 -0500

    D

--- routes/groups.js
+++ routes/groups.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var middleware = require('../middleware')
+var middleware = require('../middleware');
+const db = require('../models');
 /* Create Group */
-router.post('/create_group', middleware.protect, function(req, res, next) {
+router.post('/create_group', middleware.protect, async function(req, res, next) {
 console.log(req.body);
 console.log(req.user)
+var newGroup = await db.Group.create(req.body);
+return res.status(200).json({ statusCode: 200, message: 'Group Created Succesfully',newGroup:newGroup })
 //   GroupName: String,
 //   GroupDescription: Number,
 //   Created_on: { type: Date, default: Date.now() },
